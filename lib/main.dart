5fdaf1186cf6aa8bdf6b441393a33b2914a21333
Wendell Arnald <wendell.arnald@gmail.com>
2020-09-02 22:05:24 +0100

    Add button calculate

--- lib/main.dart
+++ lib/main.dart
@@ -41,6 +41,25 @@ class HomePage extends StatelessWidget {
             label: "Gasoline",
             ctrl: _gasolineController,
           ),
+          Container(
+            margin: EdgeInsets.all(30),
+            height: 60,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.8),
+              borderRadius: BorderRadius.circular(60),
+            ),
+            child: FlatButton(
+              child: Text(
+                "Calculate",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 35,
+                  fontFamily: "Big Shoulders Display",
+                ),
+              ),
+              onPressed: () {},
+            ),
+          ),
         ],
       ),
     );
