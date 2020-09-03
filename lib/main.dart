3882966f68b9eb37f449fedaaf448f22dde8beab
Wendell Arnald <wendell.arnald@gmail.com>
2020-09-03 22:09:05 +0100

    Return of success message

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter_masked_text/flutter_masked_text.dart';
 import 'package:fuel/widgets/input.widget.dart';
 import 'package:fuel/widgets/loading-button.widget.dart';
 import 'package:fuel/widgets/logo.widget.dart';
+import 'package:fuel/widgets/success.widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -34,35 +35,9 @@ class HomePage extends StatelessWidget {
       body: ListView(
         children: <Widget>[
           Logo(),
-          Container(
-            margin: EdgeInsets.all(30),
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.9),
-              borderRadius: BorderRadius.circular(25),
-            ),
-            child: Column(
-              children: <Widget>[
-                SizedBox(
-                  height: 50,
-                ),
-                Text(
-                  "Use Diesel",
-                  style: TextStyle(
-                    color: Theme.of(context).primaryColor,
-                    fontSize: 40,
-                    fontFamily: "Big Shoulders Display",
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                LoadingButton(
-                    busy: false,
-                    func: () {},
-                    invert: true,
-                    text: "Recalculate"),
-              ],
-            ),
+          Success(
+            reset: () {},
+            result: "Use X",
           ),
           Input(
             label: "Diesel",
