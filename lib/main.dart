062915d2f34879cff0e2cd4381a7b488a971d0ee
Wendell Arnald <wendell.arnald@gmail.com>
2020-09-08 23:06:02 +0100

    Create method for calculate

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
-import 'package:fuel/widgets/input.widget.dart';
-import 'package:fuel/widgets/loading-button.widget.dart';
-import 'package:fuel/widgets/logo.widget.dart';
-import 'package:fuel/widgets/submit-form.dart';
-import 'package:fuel/widgets/success.widget.dart';
+import 'pages/home.page.dart';
 
 void main() {
   runApp(MyApp());
@@ -21,29 +16,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  var _dieselController = new MoneyMaskedTextController();
-  var _gasolineController = new MoneyMaskedTextController();
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: ListView(
-        children: <Widget>[
-          Logo(),
-          SubmitForm(
-            dieselController: _dieselController,
-            gasolineController: _gasolineController,
-            busy: false,
-            submitFunc: () {},
-          ),
-        ],
-      ),
     );
   }
 }
